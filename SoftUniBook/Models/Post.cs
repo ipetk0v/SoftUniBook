61a1103f92ba3b59bfd3c57caaa3c7d2ef7c49e1
kimipetk0v <i.petk0v@abv.bg>
2017-04-11 23:50:57 +0300

    Add PostsController.cs and Models/Post.cs

---
+++ SoftUniBook/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class Post
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
